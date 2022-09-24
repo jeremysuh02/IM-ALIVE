e28ede1505d079a519b1a59e136acca86fe6b4a0
KJam <kaytchjam@gmail.com>
2022-09-24 17:29:48 -0500

    Added shifting script

---
+++ IM ALIVE 22/Assets/Scripts/Shifting.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shifting : MonoBehaviour
+{
+    bool shifted;
+    // Start is called before the first frame update
+    void Start() {
+        shifted = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
