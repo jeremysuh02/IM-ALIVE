0157605c69b5a9aefa30d2c90d8bf82057d49d83
jeremysuh02 <jeremysuh@utexas.edu>
2022-09-25 01:13:04 -0500

    lotta stuff
    circle enemy movement, changed player movement, started working on big skull enemy but idk what im doing

---
+++ IM ALIVE 22/Assets/Scripts/GreenCircle.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenCircle : MonoBehaviour
+{
+    [SerializeField] float Speed = 3f;
+    Rigidbody2D rb;
+    Transform target;
+    Vector2 moveDirection;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+            moveDirection = direction; 
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (target)
+        {
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * Speed;
+        }
+    }
+}
--- IM ALIVE 22/Assets/Scripts/Movement.cs
+++ IM ALIVE 22/Assets/Scripts/Movement.cs
@@ -35,8 +35,8 @@ public class Movement : MonoBehaviour {
         }
 
         // for the dash, a WIP
-        speedX = setSpeed(horizontal, speedX);
-        speedY = setSpeed(vertical, speedY);
+        //speedX = setSpeed(horizontal, speedX);
+        //speedY = setSpeed(vertical, speedY);
        
        animator.SetFloat("Speed", horizontal);
         dashDistance = 0;
@@ -51,45 +51,46 @@ public class Movement : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        Vector2 movementVector = new Vector2(horizontal * (speedX + dashDistance), vertical * (speedY + dashDistance));
+        body.velocity = new Vector2(horizontal * speed, vertical * speed);
+        /*Vector2 movementVector = new Vector2(horizontal * (speedX + dashDistance), vertical * (speedY + dashDistance));
         body.velocity = movementVector;
         //speedX = prevSpeedX;
         //speedY = prevSpeedY;
         isDashing = false;
-    }
+    } */
 
-    // Still a WIP
-    /*
-    private void dash() {
-        if (Input.GetKeyDown(KeyCode.LeftShift)) {
-            isDashing = true;
-            timeBetweenTaps = 0;
-        }
-        if (isDashing) {
-            timeBetweenTaps += Time.deltaTime;
-            if (timeBetweenTaps >= 0.1f) {
-                isDashing = false;
+        // Still a WIP
+        /*
+        private void dash() {
+            if (Input.GetKeyDown(KeyCode.LeftShift)) {
+                isDashing = true;
+                timeBetweenTaps = 0;
             }
-            body.velocity = new Vector2(horizontal * dashDistance, vertical * dashDistance);
-        }
-    }*/
+            if (isDashing) {
+                timeBetweenTaps += Time.deltaTime;
+                if (timeBetweenTaps >= 0.1f) {
+                    isDashing = false;
+                }
+                body.velocity = new Vector2(horizontal * dashDistance, vertical * dashDistance);
+            }
+        }*/
 
-    // Manages increases and decreases in speedX or SpeedY (depending on the axis)
-    private float setSpeed(float axis, float speedAxis) {
-        // Increasing / Decreasing Speed
-        if (axis != 0) {
-            speedAxis += speed * DELTA_V;
-        } else if (speedAxis > 0) {
-            speedAxis -= speed * DELTA_V * 100;
-        }
+        // Manages increases and decreases in speedX or SpeedY (depending on the axis)
+        /*private float setSpeed(float axis, float speedAxis) {
+            // Increasing / Decreasing Speed
+            if (axis != 0) {
+                speedAxis += speed * DELTA_V;
+            } else if (speedAxis > 0) {
+                speedAxis -= speed * DELTA_V * 100;
+            }
 
-        // Setting speed limits
-        if (speedAxis < 1) {
-            speedAxis = 1;
-        } else if (speedAxis > SPEED_MAX) {
-            speedAxis = SPEED_MAX;
-        }
-        return speedAxis; 
+            // Setting speed limits
+            if (speedAxis < 1) {
+                speedAxis = 1;
+            } else if (speedAxis > SPEED_MAX) {
+                speedAxis = SPEED_MAX;
+            }
+            return speedAxis; */
     } 
 }
 
