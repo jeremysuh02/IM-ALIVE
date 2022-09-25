0dc63a28af42e3c09e52c0b84fa920ddd1ba6d20
KJam <kaytchjam@gmail.com>
2022-09-25 16:43:42 -0500

    Added ArrowEnemy file type

--- IM ALIVE 22/Assets/Scripts/GreenArrow.cs
+++ IM ALIVE 22/Assets/Scripts/GreenArrow.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[SerializeField] float speed = 2f;
+Rigidbody2D rb;
+Transform target;
+Vector2 moveDirection;
+
+
 public class GreenArrow : MonoBehaviour
 {
     // Start is called before the first frame update
