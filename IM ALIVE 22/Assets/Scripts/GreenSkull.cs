07ce5b3daa8c1abd18e99b4e214e9a156ef330b4
jeremysuh02 <jeremysuh@utexas.edu>
2022-09-25 13:59:54 -0500

    movement

--- IM ALIVE 22/Assets/Scripts/GreenSkull.cs
+++ IM ALIVE 22/Assets/Scripts/GreenSkull.cs
@@ -4,36 +4,11 @@ using UnityEngine;
 
 public class GreenSkull : MonoBehaviour
 {
-    [SerializeField] float Speed = 3f;
-    Rigidbody2D rb;
-    Transform target;
-    Vector2 moveDirection;
-    // Start is called before the first frame update
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-    void Start()
-    {
-        target = GameObject.Find("GreenBottom").transform;
-    }
+    public Vector2 direction = Vector2.down;
+    [SerializeField] public float speed ;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (target)
-        {
-            Vector3 direction = (target.position - transform.position).normalized;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            rb.rotation = angle;
-            moveDirection = direction; 
-        }
-    }
-    private void FixedUpdate()
-    {
-        if (target)
-        {
-            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * Speed;
-        }
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 }
