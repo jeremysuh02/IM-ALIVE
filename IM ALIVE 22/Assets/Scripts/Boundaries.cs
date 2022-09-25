424e278302e86ca4f1ec96f5de56feeeb5fd4448
KJam <kaytchjam@gmail.com>
2022-09-24 22:06:16 -0500

    Updates done to the shift file and the boundaries file. Tags added.

--- IM ALIVE 22/Assets/Scripts/Boundaries.cs
+++ IM ALIVE 22/Assets/Scripts/Boundaries.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boundaries : MonoBehaviour
-{
+public class Boundaries : MonoBehaviour {
     // Start is called before the first frame update
+    Vector2 bounds;
     void Start()
     {
-        
+        bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, -bounds.x, bounds.x);
+        viewPos.y = Mathf.Clamp(viewPos.y, -bounds.y, bounds.y);
+        transform.position = viewPos;
     }
 }
--- IM ALIVE 22/Assets/Scripts/Shifting.cs
+++ IM ALIVE 22/Assets/Scripts/Shifting.cs
@@ -2,16 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shifting : MonoBehaviour
-{
+public class Shifting : MonoBehaviour {
+    // player shifting
     bool shifted;
+    bool shiftPressed;
+
+    // player position
+    private float playerX;
+    private float playerY;
+
+
+    // boundary variables
+    // left boundary on the left side of the screen
+    private float posBoundLeft;
+    // right boundary on the left side of the screen
+    private float posBoundRight;
+    // left boundary on the right side of the screen
+    private float negBoundLeft;
+    // right boundary on the right side of the screen
+    private float negBoundRight;
     // Start is called before the first frame update
     void Start() {
+        // all shift settings set to false by default
         shifted = false;
+        shiftPressed = false;
+        // instantiate the boundaries
+        posBoundLeft = GameObject.FindWithTag("Pos_L").transform.position.x;
+        posBoundRight = GameObject.FindWithTag("Pos_R").transform.position.x;
+        negBoundLeft = GameObject.FindWithTag("Neg_L").transform.position.x;
+        negBoundRight = GameObject.FindWithTag("Neg_R").transform.position.x;
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if (Input.GetKeyDown(KeyCode.RightShift)) {
+            shifted = !shifted;
+            shiftPressed = true;
+        }
+
+        // UPDATE POSITIONS EVERY FRAME
+        playerX = transform.position.x;
+        playerY = transform.position.y;
+
+        // check if the player is out of bounds
+        switch(shifted) {
+            // case for when you're on the left side of the screen
+            case false:
+                if (playerX < posBoundLeft) {
+                    transform.position = new Vector2(posBoundLeft + 2, playerY);
+                }
+                if (playerX> posBoundRight) {
+                    transform.position = new Vector2(posBoundRight - 2, playerY);
+                }
+                break;
+            // case for when you're on the right side of the screen
+            case true:
+                if (playerX < negBoundLeft) {
+                    transform.position = new Vector2(negBoundLeft + 2, playerY);
+                }
+                if (playerX > negBoundRight) {
+                    transform.position = new Vector2(negBoundRight - 2, playerY);
+                }
+                break;
+        }
+    }
+
+    void FixedUpdate() {
+        // update the X coordinate when "shifting" to get to the other side of the screen
+        float boundLeftNearest_X = getCurrentScreenBound(negBoundLeft, posBoundLeft, playerX);
+
+        if (shifted && shiftPressed) {
+            transform.position = new Vector2(negBoundLeft + (playerX - boundLeftNearest_X), playerY);
+        } else if (!shifted && shiftPressed) {
+            transform.position = new Vector2(posBoundLeft + (playerX - boundLeftNearest_X), playerY);
+        }
+        shiftPressed = false;
+    }
+
+    // Check which "SCREEN" the player is on
+    private float getCurrentScreenBound(float boundA_X, float boundB_X, float player_X) {
+        float screenDist = Mathf.Abs(negBoundRight - negBoundLeft);
+        if (player_X > boundA_X && player_X < boundA_X + screenDist) {
+            return boundA_X;
+        } else return boundB_X;
     }
 }
